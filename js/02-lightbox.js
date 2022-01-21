8a2438d94dc6e74f4220596357f925bb21190878
Lubov <2l-u-b-o-v@ukr.net>
2022-01-21 10:05:07 +0200

    Выводит grid изображения из массива

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,15 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryWrapper = document.querySelector('.gallery');
+const gallery = galleryItems
+  .map(
+    galleryItem => `<a class="gallery__item" href="${galleryItem.original}">
+  <img class="gallery__image" src="${galleryItem.preview}" alt="${galleryItem.description}" />
+</a>
+  `,
+  )
+  .join('');
+
+galleryWrapper.innerHTML = gallery;
